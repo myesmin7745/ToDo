4851b2dc4353150d0622083ff340b185c8d6f022
Mohona Yesmin <myesmin7745@marblehillschool.org>
2017-05-07 14:57:20 +0000

    Changes

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -3,4 +3,8 @@ class ListController < ApplicationController
     @assignment_one_description = "Complete all missed assignments - Physics...... I want to take it." 
     @assignment_one_pomodoro_estimate = "4 pomodoros"
   end
+  def first
+  end
+  def sceond
+  end
 end
