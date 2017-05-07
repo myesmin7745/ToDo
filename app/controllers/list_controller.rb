662f745b43af8ae7a0ac793319463e876ea9c496
Mohona Yesmin <myesmin7745@marblehillschool.org>
2017-05-07 14:11:10 +0000

    commit

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,4 +1,6 @@
 class ListController < ApplicationController
   def index
+    @assignment_one_description = "Complete all missed assignments - Physics" 
+    @assignment_one_pomodoro_estimate = "4 pomodoros"
   end
 end
