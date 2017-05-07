9df2280812f2bf8359677ab855db66f54f2c2aaf
Mohona Yesmin <myesmin7745@marblehillschool.org>
2017-05-07 15:16:26 +0000

    Changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'list/index'
   get 'list/homework' =>  'list#first'
-  get 'list/chords' =>  'list#second'
+  get 'list/chores' =>  'list#second'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
